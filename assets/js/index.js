e68b18fb3fc6ea290a6a69f5578c230f21cbd054
Albina <122452352+Albinosius@users.noreply.github.com>
2025-04-08 02:08:50 +0500

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll(".main__item.card");
+  const cards = document.querySelectorAll(".card");
+  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 
-  cards.forEach((card) => {
-    const bookButton = card.querySelector(".card__main-bottom-button");
+  if (cards) {
+    cards.forEach((card) => {
+      const bookButton = card.querySelector(".card__main-bottom-button");
 
-    bookButton.addEventListener("click", (event) => {
-      event.stopPropagation(); 
-      if (!card.classList.contains("pressed") && !card.classList.contains("active")) {
-        card.classList.add("pressed");
+      if (bookButton) {
+        bookButton.addEventListener("click", (event) => {
+          event.stopPropagation();
+          if (!card.classList.contains("pressed") && !card.classList.contains("active")) {
+            card.classList.add("pressed");
+
+            if (isTouchDevice) {
+                if (card.classList.contains("pressed")) {
+                  card.classList.remove("pressed");
+                  card.classList.add("active");
+                }
+            }
+          }
+        });
       }
-    });
 
-    card.addEventListener("mouseleave", () => {
-      if (card.classList.contains("pressed")) {
-        card.classList.remove("pressed");
-        card.classList.add("active");
+      if (!isTouchDevice) {
+        card.addEventListener("mouseleave", () => {
+          if (card.classList.contains("pressed")) {
+            card.classList.remove("pressed");
+            card.classList.add("active");
+          }
+        });
       }
-    });
 
-    card.addEventListener("click", () => {
-      if (card.classList.contains("active")) {
-        card.classList.remove("active");
-      }
+      card.addEventListener("click", () => {
+        if (card.classList.contains("active")) {
+          card.classList.remove("active");
+        }
+      });
     });
-  });
+  }
 });
